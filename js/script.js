47ff3a03165127e22b9a628d09485d452d36a391
Micheal Waldrup <waldrupm@gmail.com>
2020-07-10 09:55:49 -0400

    finish table loading

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+const populateSeasonSelector = () => {
+  seasonSelector = $('#seasonSelected');
+  // Cheat for functionality like python's range() function and map over.
+  seasons = [...Array(20).keys()].map(num => { return 2020 - num });
+  seasons.forEach(season => {
+    seasonOption = `<option value='${season}'>${season}</option>`;
+    seasonSelector.append(seasonOption);
+  });
+};
+
+const loadData = () => {
+  selectedSeason = $('#seasonSelected').val();
+  apiURL = `http://ergast.com/api/f1/${selectedSeason}/driverStandings.json`;
+  $.getJSON(apiURL, ( data ) => {
+    dataTableBody = $('#dataTableBody');
+    data.MRData.StandingsTable.StandingsLists[0].DriverStandings.forEach( item => {
+      
+      // destructure driver object for simplicity
+      let { familyName, givenName, nationality, url } = item.Driver;
+      
+      driverTableRow = `
+        <tr>
+          <td>${item.position}</td>
+          <td><a href="${url}">${givenName} ${familyName}</a></td>
+          <td>${item.points}</td>
+          <td>${item.wins}</td>
+          <td>${nationality}</td>
+          <td>${item.Constructors[0].name}</td>
+        </tr>
+      `; // end of driverTableRow interpolated string
+      dataTableBody.append(driverTableRow);
+    });
+  });
+}
+
+// Populate the season selector with season numbers
+populateSeasonSelector();
+
+// handle request for season data
+$('#loadDataButton').click(loadData);
